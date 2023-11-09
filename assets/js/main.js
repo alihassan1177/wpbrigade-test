4ab3315dc9242033f87dd12115ad0a7a3eb72ba8
alihassan1177 <ah927481@gmail.com>
2023-11-09 23:36:13 +0500

    design completed check for pixel perfect

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,29 +1,66 @@
-$(function(){
+$(function () {
+
+    $('a.iframe').fancybox()
+
+    // Notification Close Button
+    $(".notification-close").on("click", function () {
+        $("#notification").animate({
+            height: "toggle"
+        }, 500);
+    });
+
+
+    // Mobile Menu
+    $(".menu-toggle").on("click", function () {
+        $(".mobile-menu").animate({
+            height: "toggle"
+        }, 500, function() {
+            $(".mobile-menu").css({
+                "height" : "100vh"
+            })
+        });
+    });
+
+
+    // Hero Slider
     $('.slider').owlCarousel({
-        items : 1,
-        loop : true,
-        nav : true,
-        autoplay : true,
-        smartSpeed : 500,
-        dotsEach : true,
-        navText : [
+        items: 1,
+        loop: true,
+        nav: true,
+        autoplay: false,
+        smartSpeed: 500,
+        dotsEach: true,
+        navText: [
             '<img src="/assets/img/hero-slider-left-arrow.png" />',
             '<img src="/assets/img/hero-slider-right-arrow.png" />'
         ]
     })
 
+    // Product Slider
     $('.product-slider').owlCarousel({
-        items : 3,
-        loop : false,
-        nav : false,
-        autoplay : true,
-        smartSpeed : 500,
-        dotsEach : false,
-        navText : [
+        items: 1,
+        loop: false,
+        nav: true,
+        autoplay: true,
+        smartSpeed: 500,
+        dotsEach: false,
+        margin: 30,
+        navText: [
             '<img src="/assets/img/hero-slider-left-arrow.png" />',
             '<img src="/assets/img/hero-slider-right-arrow.png" />'
-        ]
+        ],
+        responsive: {
+            768: {
+                items: 2
+            },
+            992: {
+                items: 3
+            },
+            1200: {
+                items: 4
+            }
+        }
     })
 
-    
+
 })
